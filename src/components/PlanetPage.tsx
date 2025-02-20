d1777d5bbca218e4b7ce34afaa001f3322829aae
nk-on <2nkoz@proton.me>
2025-02-19 23:13:26 -0800

    Feat:changing Planet page layout based on different routing id's

--- src/components/PlanetPage.tsx
+++ src/components/PlanetPage.tsx
@@ -1,31 +1,56 @@
 import planets from "../Planets";
-const arr = [1,2,3,4]
+import { useParams } from "react-router";
+import PlanetMode from "./planetMode";
+import { useState } from "react";
 export default function PlanetPage() {
-  return (
-    <div className="flex flex-col justify-center items-center w-[100%] h-[100%] gap-[10px]">
+  const { planetId } = useParams();
+  const dataArr: string[] = [
+    "rotationTime",
+    "revolutionTime",
+    "radius",
+    "avgTemperature",
+  ];
+  const [planetMode, setPlanetMode] = useState<string>("overview");
+  const currentPlanet = planets.find((planet) => planet.id === Number(planetId));
+   return (<div className="flex flex-col justify-center items-center w-[100%] h-[100%] gap-[10px]">
       <div className="w-[100%] flex justify-between items-center">
-        <img src="public/assets/planet-earth.svg" alt="planet-image"></img>
+      {
+             currentPlanet?.description.map((planetDescription)=>{
+              if(Object.keys(planetDescription)[0] === planetMode){
+               return <img src={Object.values(planetDescription)[1]} />
+              }
+           })
+          }
         <div className="text-[#fff] w-[50%]">
-          <h1 className="text-[80px]">Earth</h1>
-          <p className="text-[14px]">
-            Much of the Venusian surface appears to have been shaped by volcanic activity.
-            Venus has several times as many volcanoes as Earth, and it has 167 large
-            volcanoes that are over 100 km (60 mi) across. The only volcanic complex of
-            this size on Earth is the Big Island of Hawaii.
-          </p>
-          <div className="flex flex-col"> 
-            <button className="min-w-[350px] h-[48px] border border-[#FFFFFF]">overview</button>
-            <button  className="min-w-[350px] h-[48px] border border-[#FFFFFF">internal structure</button>
-            <button  className="min-w-[350px] h-[48px] border border-[#FFFFFF">surface geology</button>
+          <h1 className="text-[80px]">{currentPlanet?.name}</h1>
+          <p className="text-[14px]"></p>
+          <div className="flex flex-col">
+            {currentPlanet?.description.map((planetMode) => (
+              <PlanetMode
+                planetMode={Object.keys(planetMode)[0]}
+                setPlanetMode={setPlanetMode}
+              />
+            ))}
           </div>
         </div>
       </div>
       <div className="flex">
-        {
-            arr.map(()=>{
-                return <div className="w-[255px] h-[128px] border border-[#FFFFFF"></div>
-            })
-        }
+        {currentPlanet?.data.map((planet, index) => {
+          const title: string = dataArr[index];
+          return (
+            <div
+              key={Number(planetId)}
+              className=" flex flex-col justify-center items-center w-[255px] h-[128px] border border-[#FFFFFF] text-[#fff]"
+            >
+              {
+                <>
+                  <h1>{title}</h1>
+                  <p>{planet[title]}</p>
+                </>
+              }
+            </div>
+          );
+        })}
       </div>
     </div>
   );
