be903fb7fbf963c7650242e2e149831fbfc555c9
nk-on <2nkoz@proton.me>
2025-02-21 21:55:51 -0800

    Feat:redirecting to the individual planet page when clicking on the menu item

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import data from "../data.json"
+import { NavLink } from "react-router";
 export default function Header() {
   return (
     <>
@@ -6,7 +7,7 @@ export default function Header() {
         <h1 className=" text-[28px]">Planets</h1>
             <ul className="flex w-[100%] md:w-[30%]  justify-between cursor-pointer">
                 {
-                  data.map((planet,index)=> <li className="uppercase text-[11px] font-700" key={index}>{planet.name}</li>)
+                  data.map((planet,index)=> <li className="uppercase text-[11px] font-700"  key={index}><NavLink to={planet.name}>{planet.name}</NavLink></li>)
                 }
             </ul>
       </div>
