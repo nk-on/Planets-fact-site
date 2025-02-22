c16dae026324fdd7389585987040dfca0c6164e1
nk-on <2nkoz@proton.me>
2025-02-21 21:17:24 -0800

    Adapting page for tablet

--- src/components/PlanetPage.tsx
+++ src/components/PlanetPage.tsx
@@ -8,15 +8,26 @@ export default function PlanetPage() {
   const [mode, setmode] = useState<string>("overview");
   const planetModes = ["overview", "structure", "geology"];
   const currentPlanet = data.find((dataElement) => dataElement.name === planetName);
-  const currentPlanetMode = currentPlanet[mode]
-  const currentPlanetImages = currentPlanet?.images
+  const currentPlanetMode = currentPlanet[mode];
+  const currentPlanetImages = currentPlanet?.images;
   return (
     <div className="flex flex-col justify-center items-center w-[100%] h-[100%] gap-[10px]">
-      <div className="w-[80%] flex justify-between items-center">
-        {<img src={currentPlanetImages[mode]} />}
-        <div className="text-[#fff] w-[50%]">
-          <h1 className="text-[80px]">{currentPlanet?.name}</h1>
-          <p className="text-[14px]">{<>{currentPlanetMode.content} <span className="flex">Source:{<Link to={currentPlanet.source}>Wikipedia</Link>}</span></>}</p>
+      <div className="w-[80%] flex flex-col md:flex-row justify-between items-center">
+        {<img src={currentPlanetImages[mode]} className="w-[184px] h-[184px] md:w-[290px] md:h-[290px] mt-[120px]"/>}
+        <div className="text-[#fff] md:w-[50%] w-[100%] gap-[60px] flex flex-row items-center justify-between md:flex-col md:items-stretch">
+          <div>
+            <h1 className="text-[80px]">{currentPlanet?.name}</h1>
+            <p className="text-[14px]">
+              {
+                <>
+                  {currentPlanetMode.content}{" "}
+                  <span className="flex">
+                    Source:{<Link to={currentPlanet.source}>Wikipedia</Link>}
+                  </span>
+                </>
+              }
+            </p>
+          </div>
           <div className="flex flex-col">
             {planetModes.map((mode) => {
               return <PlanetMode mode={mode} setPlanetMode={setmode} />;
@@ -24,7 +35,7 @@ export default function PlanetPage() {
           </div>
         </div>
       </div>
-      <div className="flex">
+      <div className="flex w-[90%] justify-center gap-[10px]">
         {dataArr.map((data, index) => {
           return (
             <div
