8fd3fb8471eab9176d3d5b494fdcdc50377e75e4
nk-on <2nkoz@proton.me>
2025-02-26 23:08:09 -0800

    Feat:adding indexes to the planet mode buttons

--- src/components/planetMode.tsx
+++ src/components/planetMode.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
-
+import { useContext } from "react";
+import { BurgerContext } from "./Context";
 interface PlanetModeProps {
   mode: string;
+  index:number,
   color: string | undefined;
   setPlanetMode: React.Dispatch<React.SetStateAction<string>>;
 }
-export default function PlanetMode({ mode, color, setPlanetMode }: PlanetModeProps) {
+export default function PlanetMode({ mode, index, color, setPlanetMode }: PlanetModeProps) {
   const [userHovered, setUserHovered] = useState<boolean>(false);
-  console.log(color);
+  const {isMobile} = useContext(BurgerContext)
   return (
     <button
-      className={`sm:min-w-[350px]   h-[48px] sm:border border-[#FFFFFF]`}
+      className={` flex justify-around items-center  h-[48px] sm:border border-[#FFFFFF] font-bold tracking-[2.57px]`}
       style={{ backgroundColor: userHovered ? color : "",}}
       key={Math.floor(Math.random() * 1000)}
       onClick={() => {
@@ -23,6 +25,9 @@ export default function PlanetMode({ mode, color, setPlanetMode }: PlanetModePro
         setUserHovered(false);
       }}
     >
+      <div>
+       {!isMobile && `0${index+1}`}
+      </div>
       {mode}
     </button>
   );
