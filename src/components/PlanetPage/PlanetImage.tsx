4589575fbe90fc2b62d459e5ff74f1edd5647b59
nk-on <2nkoz@proton.me>
2025-03-05 14:44:21 -0800

    Style:adjusting size of the planet image across different devices

--- src/components/PlanetPage/PlanetImage.tsx
+++ src/components/PlanetPage/PlanetImage.tsx
@@ -10,16 +10,16 @@ export default function PlanetImage({mode,imageOverview,currentPlanetImage}:Plan
             <div>
               <img
               src={imageOverview}
-              className="w-[184px] h-[184px] relative bottom-[120px] md:w-[290px] md:h-[290px] mt-[120px]"
+              className="w-[111px] h-[111px] sm:w-[184px] sm:h-[184px] md:w-[290px]  md:h-[184px]  relative bottom-[120px]  mt-[120px]"
             />
               <img
               src={currentPlanetImage}
-              className="w-[184px] h-[184px] absolute lg:bottom-[120px] bottom-[350px] md:w-[290px] md:h-[290px] mt-[120px]"
+              className="w-[290px] h-[290px] md:w-[184px] md:h-[184px] sm:w-[111px] sm:h-[111px]  absolute lg:bottom-[120px] bottom-[350px]  mt-[120px]"
             />
             </div> : 
             <img
               src={currentPlanetImage}
-              className="w-[184px] h-[184px] bottom-[30px] md:w-[290px] md:h-[290px] mt-[120px]"
+              className="w-[111px] h-[111px] sm:w-[184px] sm:h-[184px] md:w-[290px]   md:h-[290px]   bottom-[30px] mt-[120px]"
             />
           }
         </>
