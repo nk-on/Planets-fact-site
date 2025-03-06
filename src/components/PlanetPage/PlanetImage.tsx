3c4a262d4b6c102e2d4b5582c993336a8cabb1e7
nk-on <2nkoz@proton.me>
2025-03-05 21:22:20 -0800

    Style:bringing closer planet image to figma design

--- src/components/PlanetPage/PlanetImage.tsx
+++ src/components/PlanetPage/PlanetImage.tsx
@@ -10,11 +10,11 @@ export default function PlanetImage({mode,imageOverview,currentPlanetImage}:Plan
             <div>
               <img
               src={imageOverview}
-              className="w-[111px] h-[111px] sm:w-[184px] sm:h-[184px] md:w-[290px]  md:h-[184px]  relative bottom-[120px]  mt-[120px]"
+              className="w-[111px] h-[111px] sm:w-[184px] sm:h-[184px] md:w-[290px]  md:h-[184px]  relative bottom-[30px]  mt-[120px]"
             />
               <img
               src={currentPlanetImage}
-              className="w-[290px] h-[290px] md:w-[184px] md:h-[184px] sm:w-[111px] sm:h-[111px]  absolute lg:bottom-[120px] bottom-[350px]  mt-[120px]"
+              className="w-[129px] h-[129px] absolute lg:bottom-[100px] lg:left-[230px] bottom-[300px]  mt-[120px]"
             />
             </div> : 
             <img
