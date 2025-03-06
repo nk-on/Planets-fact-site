54ce317c26d935de822be46cf300acfacc622fb2
nk-on <2nkoz@proton.me>
2025-03-05 21:26:02 -0800

    fixing size on the image on the planetImage component

--- src/components/PlanetPage/PlanetImage.tsx
+++ src/components/PlanetPage/PlanetImage.tsx
@@ -10,11 +10,11 @@ export default function PlanetImage({mode,imageOverview,currentPlanetImage}:Plan
             <div>
               <img
               src={imageOverview}
-              className="w-[111px] h-[111px] sm:w-[184px] sm:h-[184px] md:w-[290px]  md:h-[184px]  relative bottom-[30px]  mt-[120px]"
+              className="w-[111px] h-[111px] sm:w-[184px] sm:h-[184px] md:w-[290px]   md:h-[290px]   relative bottom-[30px]  mt-[120px]"
             />
               <img
               src={currentPlanetImage}
-              className="w-[129px] h-[129px] absolute lg:bottom-[100px] lg:left-[230px] bottom-[300px]  mt-[120px]"
+              className="w-[129px] h-[129px] absolute lg:bottom-[90px] lg:left-[230px] bottom-[320px]  mt-[120px]"
             />
             </div> : 
             <img
