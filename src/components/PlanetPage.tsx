cc656e933e4862044d48fbe76f5ee9b438a59656
nk-on <2nkoz@proton.me>
2025-03-05 18:02:34 -0800

    Style:fixing styles to match figma design

--- src/components/PlanetPage.tsx
+++ src/components/PlanetPage.tsx
@@ -32,7 +32,7 @@ export default function PlanetPage() {
   return (
     <>
       {currentPlanet ? (
-        <div className="flex flex-col justify-center items-center w-[100%] h-[100%] gap-[10px]">
+        <div className="flex flex-col justify-center items-center w-[100%] h-[100%] lg:gap-[100px] gap-[50px]">
           {isMobile && (
             <div className="mt-[40vh] px-[10px] w-[100%] pt-[25px] flex justify-between text-[#fff] border-[#fff]  border-b">
               {planetModes.map((mode, index) => {
@@ -53,15 +53,15 @@ export default function PlanetPage() {
           )}
           <div className="w-[80%] flex flex-col md:flex-row justify-between items-center">
             <PlanetImage mode={mode}  imageOverview= {currentPlanet.images.overview} currentPlanetImage= {currentPlanetImage} />
-            <div className="text-[#fff] md:w-[50%] w-[100%] gap-[20px] flex flex-row items-center justify-between md:flex-col md:items-stretch">
-              <div className="text-center sm:text-left flex flex-col">
+            <div className="text-[#fff] md:w-[50%] w-[100%] gap-[20px] flex flex-row items-center justify-between  md:flex-col md:items-stretch">
+              <div className="lg:w-[120%] text-center sm:text-left flex flex-col sm:justify-center lg:items-center">
                 <h1 className="text-[80px]">{currentPlanet?.name}</h1>
-                <p className="text-[14px] sm:block flex flex-col items-center text-[#badefe] w-[350px]">
+                <div className="text-[14px] sm:block flex flex-col  items-center ml-[-15px] lg:m-auto  text-[#badefe] w-[350px]">
                   {
                     <>
                       {currentPlanetMode && typeof currentPlanetMode === "object" ? (
                         <>
-                          {currentPlanetMode.content}{" "}
+                          {currentPlanetMode.content}
                           <span className="flex text-[#6387b1] gap-[5px]">
                             Source: <span className="underline"><Link to={currentPlanetMode.source}><span className="flex justify-between items-center w-[70px]">Wikipedia <img src="public/assets/icon-source.svg" alt="source" className="w-[12px] h-[12px]" /></span></Link></span>
                           </span>
@@ -69,10 +69,9 @@ export default function PlanetPage() {
                       ) : null}
                     </>
                   }
-                </p>
-              </div>
-              {!isMobile && (
-                <div className="flex flex-col gap-[10px] w-[350px]">
+                </div>
+                {!isMobile && (
+                <div className="flex flex-col  gap-[10px] w-[350px]">
                   {planetModes.map((mode, index) => {
                     return (
                       <PlanetMode
@@ -88,6 +87,7 @@ export default function PlanetPage() {
                   })}
                 </div>
               )}
+              </div>
             </div>
           </div>
           <div className="flex flex-wrap w-[90%]  justify-center gap-[10px]">
--- src/components/PlanetPage/dataContainer.tsx
+++ src/components/PlanetPage/dataContainer.tsx
@@ -4,7 +4,7 @@ interface DataCardProps {
 }
 function DataCard({ data, value }:DataCardProps) {
   return (
-    <div className="flex flex-col justify-center items-start gap-[5px] px-[30px] py-[50px] w-[255px] h-[48px] lg:h-[88px] border border-[#FFFFFF] text-[#fff]">
+    <div className="flex flex-col justify-center items-start gap-[5px] px-[30px] py-[50px] lg:w-[255px] w-[100%] h-[48px] lg:h-[88px] border border-[#FFFFFF] text-[#fff]">
       <h1 className="text-[#838391] uppercase tracking-[1px]">{data}</h1>
       <p className="text-[40px]">{value}</p>
     </div>
